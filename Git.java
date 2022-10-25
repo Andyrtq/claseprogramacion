8b08d3c4685b09d6e34957b7bbd967dceb964335
Andyrtq <andycabrera259@gmail.com>
2022-10-25 06:14:25 +0200

    claseprogramacion

---
+++ Git.java
@@ -0,0 +1,6 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Comandos usados en Git para la presentacion del trabajo");
+        System.out.println("1.- ");
+    }
+}
---
+++ src/Git.java
@@ -0,0 +1,12 @@
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("1.-git config user.email: Este comando sirve para logearse desde el intelijj");
+        System.out.println("2.-git init: Este sirve para iniciar el programa en git");
+        System.out.println("3.-git status: Muestra el estado del directorio del trabajo ");
+        System.out.println("4.-git commit -m este sirve para poner el codigo escrito en la nube del git");
+        System.out.println("5.-git branch -M main: Sirve para organizar nuestro trabajo");
+        System.out.println("6.-git push -u origin main: Con este comando se sube todo la informacion a la " +
+                "nube del github");
+
+    }
+}
