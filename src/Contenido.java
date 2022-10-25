042ef719f5bfd9d5544183f50ac8a6fb3f6f85b6
unknown <andycabrera259@gmail.com>
2022-10-25 04:57:30 +0200

    claseprogramacion

---
+++ src/Contenido.java
@@ -0,0 +1,31 @@
+public class Contenido {
+    public static void main(String[] args) {
+        System.out.println("Conceptos basicos de programacion");
+        System.out.println("ALGORITMO; Es el conjunto de pasos para una determinada tarea.");
+        System.out.println("CODIGO; Conjunto de palabras o simbolos para darle instrucciones" +
+                "a la computadora");
+        System.out.println("LENGUAJE; Es el conjunto de definiciones para ser utilizadas en un texto" +
+                "que la computadora sea capaz de entender");
+        System.out.println("BUG; Es el problema el cual hace que un codigo no se ejectute por un " +
+                "error en el programa");
+        System.out.println("CODIGO MAQUINA; Codijo lenguaje de programacion el cual es entendible para" +
+                "la maquina");
+        System.out.println("QUERIS; Datos para una base de datos");
+        System.out.println("BACK END; Es el procesamiento detras de escena");
+        System.out.println("FULL STACK; Es la union del backend y frontend");
+        System.out.println("BACKUP; Copia de archivos de otro dispositivo" +
+                "para guardar la informacion");
+        System.out.println("BIT; Este representa solo un valor binario");
+        System.out.println("BIG DATA; Almacenamiento de grandes cantidades de informacion");
+        System.out.println("Console; ventana negra en esta se puede trabajar desde linea de comandos");
+        System.out.println("Objeto; Es la ejecucion de una clase");
+        System.out.println("Clase; Es el nombre del proyecto en el cual se va a trabajar");
+        System.out.println("Clase padre; Clase utilizada para crear otras");
+        System.out.println("Instancia; Es la ejecucion de una clase");
+        System.out.println("Hola mundo; Primer codigo ejecutado de un principiante para iniciar en el " +
+                "mundo de la programacion");
+        System.out.println("BLOB; Es el almacenamiento de grandes cantidades de datos en formato de byte");
+        System.out.println("Byte; Union de 8 bit");
+
+    }
+}
